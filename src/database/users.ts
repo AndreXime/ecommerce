ddb7816ffd92e1d517149b80f66ce989306a1b51
Andre Ximenes <andreximenes536@gmail.com>
2026-02-20 09:36:10 -0300

    refactor(types): adiciona role à interface User

--- src/database/users.ts
+++ src/database/users.ts
@@ -4,6 +4,7 @@ export interface User {
 		email: string;
 		registration: string | null;
 		phone: string | null;
+		role: "ADMIN" | "CUSTOMER" | "SUPPORT";
 		registredAt: string;
 	};
 	ordersHistory: {
